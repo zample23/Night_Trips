7c4b3fc33215c46595e0a8326354656a88a4074a
zample23 <egor33parasiuk@gmail.com>
2024-02-19 17:55:39 +0200

    Made the footer links work

--- js/main.js
+++ js/main.js
@@ -2,15 +2,15 @@
 
 const searchField = document.querySelector('.header__search-field');
 
-window.addEventListener('click', function(event) {
-   if(event.target.closest('.header__search')) {
-      searchField.classList.toggle('active');
-      searchField.value = '';
-   } else if (!event.target.closest('.header__menu')) {
-      searchField.classList.remove('active');
-      searchField.value = '';
-   }
-})
+window.addEventListener('click', function (event) {
+  if (event.target.closest('.header__search')) {
+    searchField.classList.toggle('active');
+    searchField.value = '';
+  } else if (!event.target.closest('.header__menu')) {
+    searchField.classList.remove('active');
+    searchField.value = '';
+  }
+});
 
 //-------------------------------------------------------------------//
 
@@ -19,21 +19,21 @@ window.addEventListener('click', function(event) {
 const buttons = document.querySelectorAll('.pick-your-trip__button');
 const trips = document.querySelectorAll('.trip');
 
-buttons.forEach(function(item) {
+buttons.forEach(function (item) {
+  item.addEventListener('click', function () {
+    trips.forEach(function (item) {
+      item.classList.add('hidden');
+    });
+    buttons.forEach(function (item) {
+      item.classList.remove('active');
+    });
+    const chosenProject = document.querySelector('#' + this.dataset.trip);
+    chosenProject.classList.remove('hidden');
+    this.classList.add('active');
 
-   item.addEventListener('click', function() {
-
-      trips.forEach(function(item) {
-         item.classList.add('hidden');
-      })
-      buttons.forEach(function(item) {
-         item.classList.remove('active');
-      })
-      const chosenProject = document.querySelector('#' + this.dataset.trip);
-      chosenProject.classList.remove('hidden');
-      this.classList.add('active');
-
-   })
-})
-
-
+    const chosenLink = document.querySelector(
+      `[data-trip="${this.dataset.trip}"]`
+    );
+    chosenLink.classList.add('active');
+  });
+});
